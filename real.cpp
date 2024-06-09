d8597f739117b7e512ea4bb0fed0f4ad84cc5ed6
suu9 <china001@outlook.jp>
2024-06-09 15:35:34 +0800

    Modify real.cpp

--- real.cpp
+++ real.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hit push girl feeling seek rule kitchen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
