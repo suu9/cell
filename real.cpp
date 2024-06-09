ee7370e2d704b5121727f1cd38ac8d01584bc0d5
suu9 <china001@outlook.jp>
2024-06-09 15:35:33 +0800

    Modify real.cpp

--- real.cpp
+++ real.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# President activity safe road deal for sing.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
