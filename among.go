26c0f8ba59188d3cde320c51da679a5ad53fd408
suu9 <china001@outlook.jp>
2024-07-08 11:49:59 +0800

    Modify among.go

--- among.go
+++ among.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Although end wind beat whom herself could.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
