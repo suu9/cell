18de2b865ff2d8dbea76a549aa79468634ac23e5
suu9 <china001@outlook.jp>
2024-07-11 13:04:24 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Method reach able parent meet section.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
