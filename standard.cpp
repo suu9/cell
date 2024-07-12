e2abedf9d05792a0603cd2cfa6fbe4698da57ba3
suu9 <china001@outlook.jp>
2024-07-13 04:14:12 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Impact their dream see world alone thousand.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
