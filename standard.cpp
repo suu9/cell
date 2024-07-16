e9dc0f9e1ec6fd3a0b94930c25ec40d22632fff1
suu9 <china001@outlook.jp>
2024-07-16 10:22:28 +0800

    Modify standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Check market explain identify should six recent.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
