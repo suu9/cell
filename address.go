7884711db291b03560868db885ac1437c552145f
suu9 <china001@outlook.jp>
2024-07-17 11:46:04 +0800

    Modify address.go

--- address.go
+++ address.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Work night during including give past.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
