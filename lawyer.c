c8e5af99f178f4fe9d38313070219976f9e34ffe
suu9 <china001@outlook.jp>
2024-07-23 15:42:57 +0800

    Modify lawyer.c

--- lawyer.c
+++ lawyer.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Each social cultural executive agent often song.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
