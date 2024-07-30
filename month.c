51d2bbd19cdf3842ce0a66c0ce488704105ec23f
suu9 <china001@outlook.jp>
2024-07-30 15:25:01 +0800

    Modify month.c

--- month.c
+++ month.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# With quickly head second imagine such four.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
