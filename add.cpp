a6c5da313029c46b73760b464d5ebeba9475012e
suu9 <china001@outlook.jp>
2024-08-01 12:30:55 +0800

    Modify add.cpp

--- add.cpp
+++ add.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Note course cup save while still.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
