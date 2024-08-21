7ba4ecf4d9e1533efcd04d495bafcf0c95c06eee
suu9 <china001@outlook.jp>
2024-08-21 19:51:29 +0800

    Modify industry.go

--- industry.go
+++ industry.go
@@ -17,3 +17,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Professor throw yet prepare project report.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
