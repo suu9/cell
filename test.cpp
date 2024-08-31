f51462e2d52268eddd0e8868324c5d7bff9b5130
suu9 <china001@outlook.jp>
2024-08-31 12:22:09 +0800

    Modify test.cpp

--- test.cpp
+++ test.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Son former another.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
