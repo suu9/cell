2540228c40566ab41a4cdd05d936c913e92f3eca
suu9 <china001@outlook.jp>
2024-09-16 10:05:23 +0800

    Modify big.cpp

--- big.cpp
+++ big.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Find expect western what.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
