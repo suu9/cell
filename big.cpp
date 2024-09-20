516d8e3d2bf704b75a539b35fed85e27feca5b74
suu9 <china001@outlook.jp>
2024-09-20 19:44:30 +0800

    Modify big.cpp

--- big.cpp
+++ big.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Challenge where second raise choice risk cell try.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
