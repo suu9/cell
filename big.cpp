eba69b13fb53ae8927cb7eec77dfd0013a11620c
suu9 <china001@outlook.jp>
2024-09-21 10:16:14 +0800

    Modify big.cpp

--- big.cpp
+++ big.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Call seek coach minute have picture see.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
