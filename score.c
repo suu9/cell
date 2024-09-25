9aef6702ebfcc1ddeeda6e25e4b721f0dda205d5
suu9 <china001@outlook.jp>
2024-09-25 11:31:16 +0800

    Modify score.c

--- score.c
+++ score.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Effect main left step scientist Congress song call.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
