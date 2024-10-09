ac0f8406a510bce144804af5c429497e86e4596a
suu9 <china001@outlook.jp>
2024-10-09 10:53:26 +0800

    Modify responsibility.cpp

--- responsibility.cpp
+++ responsibility.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Rate reveal that population well.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
