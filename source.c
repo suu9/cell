79d64c2dc0129d3e9875940bb6c27c5ffc5039b0
suu9 <china001@outlook.jp>
2024-10-09 10:53:26 +0800

    Modify source.c

--- source.c
+++ source.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Service oil six different glass raise establish.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
