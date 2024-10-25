1059126c129cbbb869cec7fe90df43933e6c5519
suu9 <china001@outlook.jp>
2024-10-25 14:04:34 +0800

    Modify inside.go

--- inside.go
+++ inside.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Campaign heavy school loss Mrs sing majority.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
