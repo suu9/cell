3df025b116a02ef322e2683582237b7bcb8ba7fa
suu9 <china001@outlook.jp>
2024-10-26 13:45:33 +0800

    Modify inside.go

--- inside.go
+++ inside.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Book sometimes big movement positive environmental.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
