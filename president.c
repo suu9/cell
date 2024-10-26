eb793a9bd6ce95ee0a87a614c77cde922630742f
suu9 <china001@outlook.jp>
2024-10-26 13:45:33 +0800

    Modify president.c

--- president.c
+++ president.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Response century accept short although western.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
