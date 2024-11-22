4ca9c03dc4df4f16520976ada33005912abdc9a8
suu9 <china001@outlook.jp>
2024-11-22 13:40:40 +0800

    Modify ok.cpp

--- ok.cpp
+++ ok.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Positive drop everybody.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
