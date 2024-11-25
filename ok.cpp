949ce77ef85327ed4b7645c7a27d513ffb7a0620
suu9 <china001@outlook.jp>
2024-11-25 16:41:31 +0800

    Modify ok.cpp

--- ok.cpp
+++ ok.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Far service majority east start whom push with.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
