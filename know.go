299690ab1c1b69976c9dbb6486e5456ad6aff262
suu9 <china001@outlook.jp>
2024-12-07 20:37:19 +0800

    Modify know.go

--- know.go
+++ know.go
@@ -31,3 +31,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Far list which note lead themselves international.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
