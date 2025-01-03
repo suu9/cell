b4d5da20f605f04da9cf3427cb999878960fef2f
suu9 <china001@outlook.jp>
2025-01-03 16:42:47 +0800

    Modify shake.cpp

--- shake.cpp
+++ shake.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Southern cost everybody compare analysis fact show.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
