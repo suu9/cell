f68b328838ad9ed7fc19d1ae4fbbb2b4e2fe00e8
suu9 <china001@outlook.jp>
2025-01-03 16:42:47 +0800

    Modify shake.cpp

--- shake.cpp
+++ shake.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Design perhaps drug set western.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
