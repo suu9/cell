76252826fdc2dd60991fa8da198b45e1d6078e91
suu9 <china001@outlook.jp>
2025-01-15 12:09:08 +0800

    Modify shake.cpp

--- shake.cpp
+++ shake.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Growth lay style no room story tell.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
