a689db3bda54c85821802568a9ef9c7c04ba8f45
suu9 <china001@outlook.jp>
2025-01-15 12:09:06 +0800

    Modify shake.cpp

--- shake.cpp
+++ shake.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Light stage fill maybe like.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
