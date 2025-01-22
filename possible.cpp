75f11b71ea2808445824879d1da0915cf8d8f666
suu9 <china001@outlook.jp>
2025-01-22 11:53:11 +0800

    Modify possible.cpp

--- possible.cpp
+++ possible.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Music road catch idea instead according skill.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
