d0893e95e1ed13cf2345c5aa0ac9fce6f1f04571
suu9 <china001@outlook.jp>
2025-02-21 11:42:45 +0800

    Modify detail.cpp

--- detail.cpp
+++ detail.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Every on draw provide.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
